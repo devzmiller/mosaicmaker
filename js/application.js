fb40d5acc6acb5da95a62bcd39ea5ed799391779
Devin Miller <email": null>
2017-10-04 16:18:19 -0700

    Add mouseover colors

--- js/application.js
+++ js/application.js
@@ -6,11 +6,18 @@ const colors = {
   blue: "#325f89",
   purple: "#370c6b",
   brown: "#49371f",
-  black: "#000000"
+  black: "#000000",
+  white: "#ffffff"
 }
 
 $(document).ready(function() {
+  let currentColor = null;
   $(".color-buttons").on("click", "button", (event) => {
-    let currentColor = colors[$(event.target).attr("name")];
+    currentColor = colors[$(event.target).attr("name")];
+
+    $(".cells").on("mouseover", ".cell", (event) => {
+      $(event.target).css("background-color", currentColor);
+    });
   });
+
 });
