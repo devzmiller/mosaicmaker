b7e020ad42bf4ba40bc079aa3a986e0bab0a34ea
Devin Miller <email": null>
2017-10-04 19:11:12 -0700

    Clean up files, delete unneccessary route from controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,11 +1,3 @@
 get '/' do
-
   erb :'index'
-end
-
-get '/images' do
-  screenshotter = Screencap::Fetcher.new("/")
-  @screenshot = screenshotter.fetch(div: ".cells")
-  # erb :"image"
-  File.open("/images/new_image.jpg", 'wb') { |f| f.write(@screenshot) }
 end
