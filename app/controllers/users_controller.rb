b4c799aef834093100aa23b69b46b76b4ff343a1
Devin Miller <devzmiller@gmail.com>
2017-10-08 16:15:13 -0700

    Create /users post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 get '/users/new' do
   erb :'_users_new', layout: false
 end
+
+post '/users' do
+  @user = User.create(params[:user])
+end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -7,4 +7,10 @@ describe "UserController" do
       expect(last_response).to be_ok
     end
   end
+  describe 'post /users' do
+    it 'creates a new user' do
+      post '/users', {user: {name: "bob", email: "bob@llama.com", password: "ham"}}
+      expect(User.find_by_email("bob@llama.com")).to be_a User
+    end
+  end
 end
